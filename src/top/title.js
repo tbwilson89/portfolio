8b5ed72731e8ff88b2891b361937831269eb83fb
Trevor Wilson <tbwilson@mabankisd.net>
2017-04-06 16:27:10 -0500

    Initial commit'

---
+++ src/top/title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Title extends Component {
+  render(){
+    return(
+      <div className='top-section-wrapper'>
+        <h1>Name</h1>
+        <h3>Website Visionary</h3>
+      </div>
+    )
+  }
+}
+
+export default Title;
