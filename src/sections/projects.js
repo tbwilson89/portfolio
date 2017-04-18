ba66d37c7973455c7d3448d26cf49d76b183548b
Trevor Wilson <tbwilson@mabankisd.net>
2017-04-18 16:11:32 -0500

    worked on tabs by adding codepen links and some information about backend work

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import simon from '../images/simon.PNG'
-import tictactoe from '../images/tictactoe.PNG'
-import pomodoro from '../images/pomodoro.PNG'
+import Codepen from 'react-codepen'
+import urlshorten from '../images/urlshorten.PNG'
 
 class Projects extends Component {
 
@@ -40,7 +39,7 @@ class Projects extends Component {
 
   render(){
     return(
-      <div className='project-wrapper section-border' style={{height: screen.availHeight}}>
+      <div className='project-wrapper section-border'>
         <div className='projects-tab-wrapper'>
           <div id='fcc-frontend-tab' className={this.isActive('frontend')} onClick={(e) => this.changeActive('frontend')}>
             Free Code Camp: Front End Projects
@@ -54,18 +53,51 @@ class Projects extends Component {
         </div>
 
         <div id='fcc-frontend-wrapper' className='portfolio-section'>
-          <div className='portfolio-display-box'>
-            <img src={simon} alt='Simon game'></img>
+          <div className='portfolio-display-row'>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="mRmrJx"></Codepen>
+            </div>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="dNXdwJ"></Codepen>
+            </div>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="xgxqYz"></Codepen>
+            </div>
           </div>
-          <div className='portfolio-display-box'>
-            <img src={tictactoe} alt='tic tac toe'></img>
+          <div className='portfolio-display-row'>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="xRmBEx"></Codepen>
+            </div>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="YpRGjM"></Codepen>
+            </div>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="apGrwR"></Codepen>
+            </div>
           </div>
-          <div className='portfolio-display-box'>
-            <img src={pomodoro} alt='Pomodoro Clock'></img>
+          <div className='portfolio-display-row'>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="bBmKOK"></Codepen>
+            </div>
+            <div className='portfolio-display-box'>
+              <Codepen user="tbwilson89" hash="ZBVXNm"></Codepen>
+            </div>
           </div>
         </div>
         <div id='fcc-backend-wrapper'>
-          This is where Free Code Camp Backend Projects with be listed.
+          <div className='portfolio-display-backend'>
+            <a href='https://blooming-oasis-62929.herokuapp.com/'><img src={urlshorten} alt='URL shortener Project'></img></a>
+            <p>Taking in a full HTTP URL (EX: HTTP://www.google.com) and converting it into a shorter URL by adding the url to a database and associating it with a number. Once done it displays this information to be used to translate the number and redirect.</p>
+            <p>When accessing the new short URL, it checks the number to the database and redirects the user to the webpage associated with the number.</p>
+          </div>
+          <div className='portfolio-display-backend'>
+            <a href='https://blooming-sands-60050.herokuapp.com/'>Timestamp Project</a>
+            <p>A project used to convert time from regular date to unix or from unix to a regular date.</p>
+          </div>
+          <div className='portfolio-display-backend'>
+            <a href='https://warm-wave-60757.herokuapp.com/'>Request Header Project</a>
+            <p>A project learning about pulling information from the person visiting the webpage and displaying it. The webpage displays the IP Address, language and operating system of the current user.</p>
+          </div>
         </div>
         <div id='personal-projects-wrapper'>
           This is where personal projects will go.
