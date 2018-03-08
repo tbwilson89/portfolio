15733d16adcd14d2193ba07f60bdbab467936aaa
Trevor Wilson <tbwilson@mabankisd.net>
2018-03-08 16:11:26 -0600

    Worked on floating drop down menu on hover on Nav

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
--- src/router.js
+++ src/router.js
@@ -19,13 +19,36 @@ export default class Redirects extends Component {
                 <Link to='/'><div className='link-btn'>Home</div></Link>
               </div>
               <div className='right-nav-container'>
-                <Link to='/about'><div className='link-btn'>About</div></Link>
-                <Link to='/projects'><div className='link-btn'>Projects</div></Link>
+                <Link to='/about'><div id='about-hover' className='link-btn'>About</div></Link>
+                <div className='hover-outside-div'>
+                  <div id='hover-over-this-please'>
+                    <Link to='/projects'><div className='link-btn'>Projects</div></Link>
+                  </div>
+                  <div id='hover-test' className='test-hover'>
+                    <ul>
+                      <li>
+                        <Link to='/projects/frontend'>
+                        <span className='dropdown-section-title'>
+                          <i className="fab fa-codepen"></i>
+                          <h4>Frontend Projects</h4>
+                        </span>
+                          <p>&emsp; Projects focusing on the frontend worked on through Free Code Camp and various other tutorial websites.</p>
+                        </Link>
+                      </li>
+                      <li>Test 2</li>
+                      <li>Test 3</li>
+                      <li>Test 4</li>
+                    </ul>
+                  </div>
+                </div>
                 <Link to='/contact'><div className='link-btn'>Contact</div></Link>
                 <Link to='/landgame'><div className='link-btn'>Land Game</div></Link>
                 <Link to='/social'><div className='link-btn'>Social Media</div></Link>
+
               </div>
+
             </div>
+
             <div className='display-content'>
               <Route exact path='/' component={Title}/>
               <Route path='/about' component={About}/>
